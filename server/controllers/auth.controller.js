ef363c2e962867b4c6aa5862913fc7c44f3a6df8
sneha-2510 <ranisnehaaa@gmail.com>
2024-05-16 15:52:28 +0530

    done : auth done

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model.js")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
 const handleRegister = async (req, res) => {
     try {
@@ -35,6 +36,80 @@ const handleRegister = async (req, res) => {
     }
 }
 
+const handleLogin = async (req, res) => {
+    try {
+        const user = await userModel.findOne({ email: req.body.email });
+
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: "Invalid Credentials "
+            })
+        }
+
+        if (user.role !== req.body.role) {
+            return res.status(500).send({
+                success: false,
+                message: "Role does not match."
+            })
+        }
+
+        const comparePassword = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+
+        if (!comparePassword) {
+            return res.status(500).send({
+                success: false,
+                message: "Invalid Credentials"
+            });
+        }
+
+        const token = jwt.sign({
+            userId: user._id
+        }, process.env.JWT_SECRET, {
+            expiresIn: "1d"
+        })
+
+        return res.status(200).send({
+            success: true,
+            message: "Login Successfully",
+            token,
+            user
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error in Login API",
+            error
+        })
+    }
+}
+
+//GET CURRENT USER
+const handleGetCurrentUser = async (req, res) => {
+    try {
+        const user = await userModel.findOne({ _id: req.body.userId });
+        return res.status(200).send({
+            success: true,
+            message: "User Fetched Successfully",
+            user,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send({
+            success: false,
+            message: "unable to get current user",
+            error,
+        });
+    }
+};
+
 module.exports = {
-    handleRegister
+    handleRegister,
+    handleLogin,
+    handleGetCurrentUser
 }
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const { handleRegister } = require("../controllers/auth.controller.js")
+const {
+    handleRegister,
+    handleLogin,
+    handleGetCurrentUser } = require("../controllers/auth.controller.js");
+const authMiddleWare = require("../middlewares/authMiddleWare.js");
+
 
 const router = express.Router();
 
 //register
 router.post("/register", handleRegister);
+
+//login
+router.post("/login", handleLogin)
+
+//GET CURRENT USER
+router.get("/current-user", authMiddleWare, handleGetCurrentUser)
 
 module.exports = router

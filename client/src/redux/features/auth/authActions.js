945e5314cc30a21b791187adb2344a662ca80ae8
Suman-sadhu-khan <su2222man@gmail.com>
2024-05-17 12:49:36 +0530

    register page updated

--- client/src/redux/features/auth/authActions.js
+++ client/src/redux/features/auth/authActions.js
@@ -52,7 +52,7 @@ export const userRegister = createAsyncThunk(
         organisationName,
         address,
         hospitalName,
-        website,
+        // website,
       });
       if (data?.success) {
 
